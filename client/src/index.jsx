6670fbeb54e09b07fd9bef645a6dc5cc7ae21573
unknown <iketart2@gmail.com>
2021-11-19 21:50:19 -0700

    init

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,44 +7,17 @@ import axios from 'axios'
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      text: '',
-      author: ''
-    }
-    this.getQuotes = this.getQuotes.bind(this)
-  }
-
-componentDidMount() {
-  this.getQuotes()
-}
  
-getQuotes = () => {
-  axios.get('/a')
-    .then(response => {
-      var info = response.data
-
-        this.setState({
-          text: info[0].text,
-          author: info[0].author
-      });
-  })
-  .catch(error => {
-    console.log(error)
-  });
-
-}
-
   render() {
     return (
       
       <div>
-        <h1> Inspirational Quote of the Day!</h1>
-        <button onclick={this.getQuotes}> sd </button>
-           {this.state.text}
+        <h1> BOILER PLATE</h1>
            </div>
 
       )
   }
+}
 }
 
 ReactDOM.render(<App />,document.getElementById('App'));
