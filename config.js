c05f987bb2290102e9d1dd67c545c2a7121f4a6b
Chris Monteilh <chris@sunsationalswimschool.com>
2021-11-21 22:49:47 -0800

    Add config.js for Firebase

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  DB_APIKEY: 'AIzaSyC79g7ryLV7CSvcpYJ-6YzHIov9IglM3Vk',
+  DB_AUTHDOMAIN: 'netflux-82acf.firebaseapp.com',
+  DB_PROJECTID: 'netflux-82acf',
+  DB_STORAGEBUCKET: 'netflux-82acf.appspot.com',
+  DB_MESSAGINGSENDERID: '557072917948',
+  DB_APPID: '1:557072917948:web:91d5cf8b0eb3993a6af7b9'
+}
